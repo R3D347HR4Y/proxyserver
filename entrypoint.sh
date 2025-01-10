c0f2abd832ae62679b849bfdc5910fab51a45923
Eliott Guillaumin <president@heavensight.com>
2025-01-11 00:01:12 +0100

    Ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,13 @@ sleep 5  # Give tailscaled time to initialize
 
 # Authenticate Tailscale (use authkey for unattended setups)
 echo "Authenticating Tailscale..."
-tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=${TAILSCALE_HOSTNAME:-"haproxy-server"}
+if [ -n "$TAILSCALE_AUTH_KEY" ]; then
+    tailscale up --authkey=${TAILSCALE_AUTH_KEY} --hostname=${TAILSCALE_HOSTNAME:-"haproxy-server"}
+else
+    echo "Error: TAILSCALE_AUTH_KEY not provided."
+    exit 1
+fi
+
 # Verify Tailscale is running
 tailscale status
 
